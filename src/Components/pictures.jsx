7d7f060f3ce15fcb9814b49aeaf1592fc87e0116
seemapoudel58 <simapoudel6@gmail.com>
2024-04-06 15:26:48 +0545

    picture ui completed

--- src/Components/pictures.jsx
+++ src/Components/pictures.jsx
@@ -4,13 +4,15 @@ const Pictures = ({ photoList }) => {
   return (
     <div>
       <h3 className="mt-[1rem] text-3xl font-semibold">Pictures</h3>
-      {photoList.map((photo, index) => {
-        return (
-          <div key={index}>
-            <img src={photo.urls.regular} />
-          </div>
-        );
-      })}
+      <ul className="grid grid-cols-2 mt-[2rem] gap-5 max-w-[80vw] md:grid-cols-3 lg:grid-cols-4 mx-auto">
+        {photoList.map((photo, index) =>{
+          return(
+            <li key={index} className='h-[10rem] rounded overflow-hidden shadow-lg transition-transform duration-300 ease-in-out   hover:scale-105  cursor-pointer md:h-[14rem] lg:h-[20rem]'>
+              <img src={photo.urls.regular} className="h-[100%]  w-[100%] object-cover" />
+            </li>
+          )
+        })}
+      </ul>  
     </div>
   );
 };
